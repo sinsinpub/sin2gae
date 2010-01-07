c5338be932b26e30642f7fa73f6eb9b841008ca2
sinsinpub <sinsinpub@251f673c-c833-11de-8672-e51b9359e43c>
2010-01-07 10:45:39 +0000

    missed file added

---
+++ trunk/app/KinkoraBot/dologin.py
@@ -0,0 +1,17 @@
+﻿#!/usr/bin/env python
+
+from google.appengine.ext import db
+from google.appengine.api import users
+from django.utils import simplejson
+import string
+import random
+import time
+import os
+import re
+import twitter
+import timet
+import rept
+import randt
+
+def doLogin():
+  return twitter.Api(username='yourbotname', password='yourbotpassword')
