b6a76c8b8d86cfa332e4652fdb9ae1afb521aeab
minamimaster@gmail.com <minamimaster@gmail.com@251f673c-c833-11de-8672-e51b9359e43c>
2010-12-05 10:22:04 +0000

    更新字串超过140自动截断

--- trunk/app/MinamiBot4GAE/tweetPost.py
+++ trunk/app/MinamiBot4GAE/tweetPost.py
@@ -14,16 +14,20 @@ TWITTER_ACCESS_TOKEN_SECRET =  ''
 # 将文本文件中的一行随机发布到推特
 class TweetPost(webapp.RequestHandler):
 	def get(self):
-		#~ f = file('data.txt')
-		#~ list_of_all_the_lines = list(f) # 读取所有行到一个列表，保留每行的换行符
-		#~ postText = random.choice(list_of_all_the_lines)
 		f = open("data.txt", "r")
-		list_of_all_the_lines = [l.strip() for l in f.readlines()] #读取所有行到列表，清除换行符
-		postText = random.choice(list_of_all_the_lines)
+		list_of_all_the_lines = [l.strip() for l in f.readlines()]
+		#确保正确关闭文件
 		try:
 			all_the_text = f.read()
 		finally:
 			f.close()
+		postText = random.choice(list_of_all_the_lines)
+		#~ postText2 = unicode(postText, "utf-8") #省略参数将用python默认的ASCII来解码
+		postText3 = postText.decode("utf-8") #把str转换成unicode是decode，unicode函数作用与之相同
+		if (len(postText3) > 140): #如果大于140个字长则裁切到140以内
+			postText4 = postText.decode('utf8')[:137].encode('utf8') + '...'#先转换成unicode，再取子串，然后转换成utf-8
+		else:
+			postText4 = postText
 		client = oauth.TwitterClient(TWITTER_CONSUMER_KEY,
 							TWITTER_CONSUMER_SECRET, None)
 		param = {'status': postText}
