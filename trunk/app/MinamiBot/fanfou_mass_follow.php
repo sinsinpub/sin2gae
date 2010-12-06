1c027329cc6139d2b7e4dcfd46da519a53db53bf
minamimaster@gmail.com <minamimaster@gmail.com@251f673c-c833-11de-8672-e51b9359e43c>
2010-12-06 10:21:27 +0000

    修正关注错误

--- trunk/app/MinamiBot/fanfou_mass_follow.php
+++ trunk/app/MinamiBot/fanfou_mass_follow.php
@@ -34,16 +34,7 @@ function convert_query($userstofollow)
 // 随机获取一个ID的某一个foer或foing
 function one_to_follow($username, $password, $target)
 {
-    $rand_num = rand(1, 2);
-    if (rand_num == 1)
-    {
-        $foing_or_foer = 'followers';
-    }
-    else
-    {
-        $foing_or_foer = 'friends';
-    }
-    $userstoprefollow = simplexml_load_file("http://$username:$password@api2.fanfou.com/$foing_or_foer/ids/$target.xml");
+    $userstoprefollow = simplexml_load_file("http://$username:$password@api2.fanfou.com/followers/ids/$target.xml");
     $userstoprefollow = (array)$userstoprefollow;
     $userstoprefollow2 = convert_query($userstoprefollow);
     $rand_key = array_rand($userstoprefollow2);
@@ -72,26 +63,35 @@ foreach ($userstounfollow as $usertounfollow)
 // 关注用户
 $usertoprefollow0 = one_to_follow($username, $password, $username);
 
-$rand_num = rand(1, 2);
-if (rand_num == 1)
-{
-    $foing_or_foer = 'followers';
-}
-else
-{
-    $foing_or_foer = 'friends';
-}
-$userstofollow = simplexml_load_file("http://$username:$password@api2.fanfou.com/$foing_or_foer/ids/$usertoprefollow0.xml");
+$userstofollow = simplexml_load_file("http://$username:$password@api2.fanfou.com/followers/ids/$usertoprefollow0.xml");
 $userstofollow = (array)$userstofollow;
 $userstofollow2 = array();
 $userstofollow2 = convert_query($userstofollow);
-if (count($userstofollow2) > 0) // 如果用户数不为0
+if (count($userstofollow2) > 100) // 如果用户数多于100个
 {
     // 仅最多关注100个用户
     $rand_keys = array_rand($userstofollow2, 100);
     for ($i=0; $i<=99; $i++)
     {
         $usertofollow = $userstofollow2[$rand_keys[$i]];
+        // 如果对方关注了我，则不关注
+        if (@simplexml_load_file("http://$username:$password@api2.fanfou.com/friendships/exists.xml?user_a=$usertofollow&user_b=$username") == "true")
+        {
+            echo $usertofollow ." 已经关注了你，不必再关注\n";
+        }
+        else
+        {
+            // 如果对方没有关注我，则关注
+            $output = do_post_request("http://$username:$password@api2.fanfou.com/friendships/create.xml?id=$usertofollow");
+            $output = simplexml_load_string($output);
+            echo '你刚刚关注了: '. $usertofollow ."\n";
+        }
+    }
+}
+else
+{
+    foreach ($userstofollow2 as $usertofollow)
+    {
         // 如果对方关注了我，则不关注
         if (@simplexml_load_file("http://$username:$password@api2.fanfou.com/friendships/exists.xml?user_a=$usertofollow&user_b=$username") == "true")
         {
