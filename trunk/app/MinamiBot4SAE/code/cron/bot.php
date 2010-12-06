34824930a85eae38a117e0a91a054275fbad021b
minamimaster@gmail.com <minamimaster@gmail.com@251f673c-c833-11de-8672-e51b9359e43c>
2010-12-06 06:58:52 +0000

    更新140字截断（未测试）

--- trunk/app/MinamiBot4SAE/code/cron/bot.php
+++ trunk/app/MinamiBot4SAE/code/cron/bot.php
@@ -2,6 +2,32 @@
 
 include_once( 'weibo.sae.class.php' );
 
+function utf8Substr($str, $from, $len){
+    return preg_replace('#^(?:[\x00-\x7F]|[\xC0-\xFF][\x80-\xBF]+){0,'.$from.'}'.
+                       '((?:[\x00-\x7F]|[\xC0-\xFF][\x80-\xBF]+){0,'.$len.'}).*#s',
+                       '$1',$str);
+}
+
+function strlen_utf8($str) {
+    $i = 0;
+    $count = 0;
+    $len = strlen ($str);
+    while ($i < $len) {
+        $chr = ord ($str[$i]);
+        $count++;
+        $i++;
+        if($i >= $len) break;
+        if($chr & 0x80) {
+            $chr <<= 1;
+            while ($chr & 0x80) {
+                $i++;
+                $chr <<= 1;
+            }
+        }
+    }
+    return $count;
+}
+
 $w = new weibo( '********' );
 $w->setUser( '********' , '********' );
 $filename = 'data.txt';
@@ -10,6 +36,10 @@ if ($fh = fopen($filename, "r")) {
 	$randpost = array_rand($putfile);
 	$postTweet = str_replace("\r\n","",$putfile[$randpost]);
 	fclose($fh);
+    if (strlen_utf8($postTweet) > 140 )
+    {
+        $postTweet = utf8Substr($postTweet, 0, 137).'...';
+    }
 } else {
 	echo '<h1>Open is not exist.</h1>';
 }
